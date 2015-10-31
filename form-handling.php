a911702044f35f70d645cd5a7d170439775df0d2
charlotteec <blipbleepbloop@gmail.com>
2015-10-31 14:00:34 -0700

    testing php form handling

--- form-handling.php
+++ form-handling.php
@@ -1,19 +1,7 @@
 <html>
-<title>
-Believe In Yourself
-</title>
-<head>
-  <link rel="stylesheet" type="text/css" href="stylesheet.css">
-  <script src="javascript.js"></script>
-</head>
 <body>
-  <header>
-    <a href="./index.html" class="link">Home</a>
-    <a href="./inspiration.html" class="link">Inspiration</a>
-    <a href="./submit.html" class="link">Submit</a>
-  </header>
-<p class="text">
-  Thank you for your submission
-  <?php echo $_POST["msg"]; ?>
+
+Welcome <?php echo $_POST["msg"]; ?>
+
 </body>
 </html>
